8a4db2fd9dc98860a8fa78b0406fdb7c24a97b6e
Jake Torres <jakert2244@gmail.com>
2023-08-23 11:01:05 -0700

    Initial Commit

---
+++ Mango/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mango
+//
+//  Created by Jake Torres on 8/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
